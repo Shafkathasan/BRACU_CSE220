b60a97d455c8656fb1f263f16614e95f55bb0c04
Shafkat Hasan <shafkathasan2@gmail.com>
2020-02-04 22:16:39 +0600

    Add Assignments-01

---
+++ CSE220_Assignments/Assignment-01/Solve/Task01.java
@@ -0,0 +1,246 @@
+//Name: Shafkat Hasan
+//ID: 19101077
+//Lab 01 : Liner Array
+package Assignment_01;
+
+public class Task01 {
+
+    public static void main(String[] args) {
+        int x = (1910%33) + (1077%34) ;
+        int[] a = {x-18, x+4, x, x+14, x-5, x+4, x-2, x+9};
+
+        System.out.println("\n///// TEST 01: Copy Array example /////");
+        int[] b = copyArray(a, a.length);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+
+//        System.out.println("\n///// TEST 02: Print Reverse example /////");
+//        printArray(a); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+//        printReverse(a); // This Should Print: { 61, 50, 56, 47, 66, 52, 56, 34 } 
+
+        System.out.println("\n///// TEST 02: Resize Array example /////");
+        b = copyArray(a, a.length);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        b = resizeArray(b, 10);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61, 0, 0 }
+
+        System.out.println("\n///// TEST 03: Reverse Array example /////");
+        b = copyArray(a, a.length);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        reverseArray(b);
+        printArray(b); // This Should Print: { 61, 50, 56, 47, 66, 52, 56, 34 } 
+
+        System.out.println("\n///// TEST 04: Shift Left k cell example /////");
+        b = copyArray(a, a.length);
+        printArray(b);
+        b = shiftLeft(b, 3);
+        printArray(b); // This Should Print: { 66, 47, 56, 50, 61, 0, 0, 0 } 
+
+        System.out.println("\n///// TEST 05: Shift Right k cell example /////");
+        b = copyArray(a, a.length);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        b = shiftRight(b, 3);
+        printArray(b);  // This Should Print: { 0, 0, 0, 34, 56, 52, 50, 61 } 
+
+        System.out.println("\n///// TEST 06: Rotate Left k cell example /////");
+        b = copyArray(a, a.length);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        b = rotateLeft(b, 4);
+        printArray(b); // This Should Print: { 61, 50, 56, 47, 66, 52, 56, 34 } 
+
+        
+        System.out.println("\n///// TEST 07: Rotate Right k cell example /////");
+        b = copyArray(a, a.length);
+        printArray(b); // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        b = rotateRight(b, 4);
+        printArray(b); // This Should Print: { 47, 56, 50, 61, 34, 56, 52, 66 } 
+
+        System.out.println("\n///// TEST 08: Insert example 1 /////");
+
+        b = copyArray(a, a.length);
+        printArray(b);  // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        boolean bol = insert(b, 9, 70, 3); // This Should Print: No space Left 
+        System.out.println(bol); // This Should Print: false
+        printArray(b);  // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+
+        System.out.println("\n///// TEST 09: Insert example 2 /////");
+        b = copyArray(a, a.length);
+        printArray(b);  // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 } 
+        bol = insert(b, 5, 70, 2);  // This Should Print: Number of elements after insertion: 6
+        System.out.println(bol); // This Should Print: true
+        printArray(b); // This Should Print: { 34, 56, 70, 52, 66, 47, 56, 50 } 
+
+        System.out.println("\n///// TEST 10: Insert example 3 /////");
+        b = copyArray(a, a.length);
+        printArray(b);  // This Should Print: { 34, 56, 52, 66, 47, 56, 50, 61 }  
+        bol = insert(b, 6, 70, 6); // This Should Print: Number of elements after insertion: 7 
+        System.out.println(bol); // This Should Print: true
+        printArray(b); // This Should Print: { 10, 20, 70, 30, 40, 50, 70 } 
+
+        System.out.println("\n///// TEST 11: Remove example 1 /////");
+
+        printArray(b); // This Should Print: { 10, 20, 70, 30, 40, 50, 70 } 
+        bol = removeAll(b, 7, 90);
+        System.out.println(bol); // This Should Print: false
+        printArray(b); // This Should Print: { 10, 20, 70, 30, 40, 50, 70 } 
+
+        System.out.println("\n///// TEST 12: Remove example 2 /////");
+        printArray(b);  // This Should Print: { 10, 20, 70, 30, 40, 50, 70 } 
+        bol = removeAll(b, 7, 70);
+        System.out.println(bol); // This Should Print: true
+        printArray(b);  // This Should Print: { 10, 20, 30, 40, 50, 0, 0 } 
+
+    }
+
+    // Prints the contents of the source array
+    public static void printArray(int[] source) {
+        for (int i = 0; i < source.length; i++) {
+            if (i == source.length - 1) {
+                System.out.print(source[i] + "\n");
+            } else {
+                System.out.print(source[i] + ", ");
+            }
+        }
+    }
+
+//    // Prints the contents of the source array in reverse order
+//    public static void printReverse(int[] source) {
+//        for (int i = source.length - 1; i >= 0; i--) {
+//            if (i == 0) {
+//                System.out.print(source[i] + "\n");
+//            } else {
+//                System.out.print(source[i] + ", ");
+//            }
+//        }
+//    }
+
+    public static int[] resizeArray(int[] source,int newcapacity) {
+        int[] T = new int[newcapacity];
+        for (int i = 0; i <source.length; i++) {
+            T[i] = source[i];
+        }
+        return T;
+    }
+    
+    // creates a copy of the source array and returns the reference of the newly created copy array
+    public static int[] copyArray(int[] source, int len) {
+        int[] c = new int[len];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = source[i];
+        }
+        return c;
+    }
+
+    // creates a reversed copy of the source array and returns the reference of the newly created reversed array
+    public static void reverseArray(int[] source) {
+        int i, T;
+        for (i = 0; i < source.length / 2; i++) {
+            T = source[i];
+            source[i] = source[source.length - 1 - i];
+            source[source.length - 1 - i] = T;
+        }
+    }
+
+    // Shifts all the elements of the source array to the left by 'k' positions
+    // Returns the updated array
+    public static int[] shiftLeft(int[] source, int k) {
+        for (int i = 0; i + k < source.length; i++) {
+            source[i] = source[i + k];
+            source[i + k] = 0;
+        }
+        return source;   
+    }
+
+    // Shifts all the elements of the source array to the right by 'k' positions
+    // Returns the updated array
+    public static int[] shiftRight(int[] source, int k) {
+        for (int i = 0; i < k; i++) {
+            source[i + k] = source[i];
+            source[i] = 0;
+        }
+        return source;    
+    }
+
+    // Rotates all the elements of the source array to the left by 'k' positions
+    // Returns the updated array
+    public static int[] rotateLeft(int[] source, int k) {
+        for (int i = 0; i + k < source.length; i++) {
+            int T = source[i];
+            source[i] = source[i + k];
+            source[i + k] = T;
+        }
+        return source;   
+    }
+
+    // Rotates all the elements of the source array to the right by 'k' positions
+    // Returns the updated array
+    public static int[] rotateRight(int[] source, int k) {
+        for (int i = source.length - 1; i - k >= 0; i--) {
+            int T = source[i];
+            source[i] = source[i - k];
+            source[i - k] = T;
+        }
+        return source;
+    }
+
+    /**
+     * @return true if insertion is successful; @return false otherwise
+     * @param arr the reference to the linear array
+     * @param size the number of elements that exists in the array.
+     * size<=arr.length @para m elem the element that is to be inserted @param
+     * index the index in which the element is required to be inserted if
+     * insertion is not successful due to lack space, print "No space Left" if
+     * given index is invalid, print "Invalid Index" if insertion is successful,
+     * print the 'Number of elements after insertion' is completed
+     */
+    public static boolean insert(int[] arr, int size, int elem, int index) {
+        if (size <= arr.length) {
+            for (int i = arr.length - 1; i > index; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[index] = elem;
+            System.out.println("Number of elements after insertion: " + (size + 1));
+            return true;
+        } else {
+            System.out.println("No space Left");
+            return false;
+        }
+    }
+
+    /**
+     * removes all the occurences of the given element
+     *
+     * @param arr the reference to the linear array
+     * @param size the number of elements that exists in the array.
+     * size<=arr.length @param elem the element to be removed @return true if
+     * removal is successful; return fal se otherwise if removal is successful,
+     * print the 'Number of elements after removal' is completed
+     */
+    public static boolean removeAll(int[] arr, int size, int elem) {
+        boolean check = false;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == elem) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            check = false;
+        } else {
+            check = true;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == elem) {
+                    for (int j = i; j < arr.length - 1; j++) {
+                        arr[j] = arr[j + 1];
+                        size--;
+                    }
+                }
+            }
+            int length = arr.length - 1;
+            for (int i = 0; i < size; i++) {
+                arr[length] = 0;
+                length--;
+            }
+        }
+        return check;
+    }
+}

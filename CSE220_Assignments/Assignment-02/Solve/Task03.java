552a5990f34dfbf157696d2cb99b4ea1c7011371
Shafkat Hasan <shafkathasan2@gmail.com>
2020-02-06 20:42:24 +0600

    Update Assignment-02 Task-03

--- CSE220_Assignments/Assignment-02/Solve/Task03.java
+++ CSE220_Assignments/Assignment-02/Solve/Task03.java
@@ -3,12 +3,15 @@
 //Assignment-02 : Circular Array
 package Assignment_02;
 
+import java.util.Random;
+
 public class Task03 {
-    
+
     private int start;
     private int size;
     private Object[] cir;
-    
+    Random random = new Random();
+
     public Task03(Object[] arr, int st, int sz) {
         start = st;
         size = sz;
@@ -18,7 +21,7 @@ public class Task03 {
 //            cir[(start + i) % arr.length] = arr[i];
 //        }
     }
-    
+
     //Prints from index --> 0 to cir.length-1
     public void printFullLinear() {
         for (int i = 0; i <= cir.length - 1; i++) {
@@ -29,7 +32,7 @@ public class Task03 {
             }
         }
     }
-    
+
     // Starts Printing from index start. Prints a total of size elements
     public void printForward() {
         for (int i = 0; i < size; i++) {
@@ -41,7 +44,7 @@ public class Task03 {
         }
         System.out.println("\n");
     }
-    
+
     public void printBackward() {
         for (int i = size - 1; i >= 0; i--) {
             if (i == 0) {
@@ -52,7 +55,7 @@ public class Task03 {
         }
         System.out.println("\n");
     }
-    
+
     // With no null cells
     public void linearize() {
         Object[] T = new Object[size];
@@ -60,5 +63,40 @@ public class Task03 {
             T[i] = cir[(start + i) % cir.length];
         }
         cir = T;
+    }
+
+    // Start index becomes zero
+    public void resize(int newcapacity) {
+        Object[] T = new Object[newcapacity];
+        for (int i = 0; i < size; i++) {
+            T[i] = cir[(start + i) % cir.length];
+        }
+        for (int i = size; i < T.length; i++) {
+            T[i] = random.nextInt(100); // Generate random integers in range 0 to 99 
+        }
+        cir = T;
+    }
+
+    public void insert(Object elem, int pos) {
+
+        if (size >= cir.length) {
+            resize(size + 3);
+        }
+        int index = (start + pos) % cir.length;
+        for (int i = size; i > pos; i--) {
+            cir[(start + i) % cir.length] = cir[(start + i - 1) % cir.length];
+        }
+        cir[index] = elem;
+    }
+
+    public void remove(int pos) {
+        int index = (start + pos) % cir.length;
+        for (int k = 0; k < size - pos; k++) {
+            cir[index] = cir[(index + 1) % cir.length];
+            index++;
+            if (index >= cir.length) {
+                index = index % cir.length;
+            }
+        }
     }
 }
--- CSE220_Assignments/Assignment-02/Solve/Task03_Test.java
+++ CSE220_Assignments/Assignment-02/Solve/Task03_Test.java
@@ -7,15 +7,38 @@ public class Task03_Test {
     public static void main(String[] args) {
         int x = (1077%34) + (1077%33);
         Object arr [] = {x+50,x+4,null,null,x,x+10,x-10,x-18}; //{94, 48, null, null, 44, 54, 34, 26}
-        
+        System.out.println("///// Array /////");
         Task03 c = new Task03(arr, 4, 6);
         c.printFullLinear();
         
+        System.out.println("///// Forward Print /////");
         c.printForward();
         
+        System.out.println("///// Backward Print /////");
         c.printBackward();
         
+        System.out.println("///// Linearize /////");
         c.linearize();
+        c.printFullLinear();
+        
+        System.out.println("///// Resize /////");
+        c.resize((1077%5)+(1077%3)+8); //(1077%5)+(1077%3)+8 = 10;
+        c.printFullLinear();
+        
+        System.out.println("///// Insert-01 /////");
+        c.insert(0, 077%6); // 077%6 = 3;
+        c.printFullLinear();
+        
+        System.out.println("///// Insert-02 /////");
+        c.insert(0, 077%5); // 077%5 = 3;
+        c.printFullLinear();
+        
+        System.out.println("///// Remove-01 /////");
+        c.remove(077%6);
+        c.printFullLinear();
+        
+        System.out.println("///// Remove-02 /////");
+        c.remove(077%5);
         c.printFullLinear();
     }
 }
